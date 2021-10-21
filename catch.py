bb1062023cece2511a044eacf62fe7467aa19aab
rayray2002 <rayray2002.huang@gmail.com>
2021-10-21 22:48:37 +0800

    log improve

--- catch.py
+++ catch.py
@@ -1,5 +1,6 @@
 from driver import *
 from pyvirtualdisplay import Display
+from datetime import datetime
 
 display = Display(visible=False, size=(1920, 1080))
 display.start()
@@ -7,12 +8,14 @@ display.start()
 config = configparser.ConfigParser()
 config.read('config.ini', 'utf8')
 
-with open('token.txt', 'r') as tokens:
-    tokens = tokens.read().splitlines()
+with open('token.txt', 'r') as users:
+    users = users.read().splitlines()
 
-for token in tokens:
+for user in users:
+    name, token = user.split(':')
+    present = datetime.now()
     catcher = AutoCatcher(config, config['default']['server_url'], token)
-    catcher.send_message('Start Catching', True)
+    catcher.send_message(f'{name} starts catching at {present.strftime("%H:%M:%S")}', True)
 
     flag = True
     while flag:
